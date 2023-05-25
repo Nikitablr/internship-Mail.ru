d866581a09e2306f4cd8fccfdc1841d7efc4362f
nikrud <89258843+nikrud@users.noreply.github.com>
2023-05-25 13:42:58 +0200

    added new method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 class BasePage:
 
     def __init__(self, browser):
@@ -6,4 +10,12 @@ class BasePage:
     def go_to_site(self):
         url = "https://mail.ru/"
         self.browser.get(url)
+
+    def click(self, *locator):
+        button = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(locator))
+        button.click()
+
+
+
+
 
