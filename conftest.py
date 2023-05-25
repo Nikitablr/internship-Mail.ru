f9057273c05e1de5c812321bdc130badff8f28b0
nikrud <89258843+nikrud@users.noreply.github.com>
2023-05-25 12:06:06 +0200

    deleted Chrome Manager

--- conftest.py
+++ conftest.py
@@ -1,11 +1,10 @@
 import pytest
 from selenium import webdriver
-import time
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(scope="function")
 def browser():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome()
     driver.implicitly_wait(10)
     yield driver
     driver.close()
