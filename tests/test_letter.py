7734bd5ff91ac84d340a133af90590bd351054bb
nikrud <89258843+nikrud@users.noreply.github.com>
2023-05-25 11:08:42 +0200

    change name of instance

--- tests/test_letter.py
+++ tests/test_letter.py
@@ -2,21 +2,21 @@ from pages.main_page import MainPage
 from pages.mail_page import MailPage
 
 def test_add_letter_to_draft(browser):
-    page = MainPage(browser)
-    page.go_to_site()
-    page.click_login_button()
-    page.click_allow_button()
-    page.fill_email_address_field()
-    page.click_enter_password_button()
-    page.enter_password_button()
-    page.click_login_button_password()
-    page = MailPage(browser)
-    page.click_create_message()
-    page.enter_email()
-    page.click_save_draft()
-    page.click_close_message_window()
-    page.open_draft_page()
-    page.message_in_draft()
+    main_page = MainPage(browser)
+    main_page.go_to_site()
+    main_page.click_login_button()
+    main_page.click_allow_button()
+    main_page.fill_email_address_field()
+    main_page.click_enter_password_button()
+    main_page.enter_password_button()
+    main_page.click_login_button_password()
+    mail_page = MailPage(browser)
+    mail_page.click_create_message()
+    mail_page.enter_email()
+    mail_page.click_save_draft()
+    mail_page.click_close_message_window()
+    mail_page.open_draft_page()
+    mail_page.message_in_draft()
 
 
 
