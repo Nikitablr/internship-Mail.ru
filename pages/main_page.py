30e9111a4cf7e189076153fc00207bb42b8ccecc
nikrud <89258843+nikrud@users.noreply.github.com>
2023-05-25 11:52:48 +0200

    added locators to the page class

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,28 +1,38 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
-from locators import locators
+
 
 
 class MainPage(BasePage):
+    LOGIN_BUTTON = (By.CSS_SELECTOR, '.ph-login')
+    ALLOW_BUTTON = (By.CSS_SELECTOR, '.cmpboxbtn.cmpboxbtnyes')
+    LOGIN_FRAME = (By.CSS_SELECTOR, ".ag-popup__frame__layout__iframe")
+    EMAIL_FIELD = (By.NAME, 'username')
+    ENTER_PASSWORD_BUTTON = (By.CSS_SELECTOR, '.submit-button-wrap')
+    PASSWORD_FIELD = (By.CSS_SELECTOR, '.withIcon-0-2-72')
+    LOGIN_BUTTON_PASSWORD = (By.CSS_SELECTOR, '.inner-0-2-81')
+
+
     #Open login page
     def click_login_button(self):
-        self.browser.find_element(*locators.LOGIN_BUTTON).click()
+        self.browser.find_element(*MainPage.LOGIN_BUTTON).click()
     #Accept cookies
     def click_allow_button(self):
-        self.browser.find_element(*locators.ALLOW_BUTTON).click()
+        self.browser.find_element(*MainPage.ALLOW_BUTTON).click()
 
     def fill_email_address_field(self):
-        self.browser.switch_to.frame(self.browser.find_elements(By.CSS_SELECTOR, ".ag-popup__frame__layout__iframe")[0])
-        self.browser.find_element(*locators.EMAIL_FIELD).send_keys("costartrial")
+        frame = self.browser.find_element(*MainPage.LOGIN_FRAME)
+        self.browser.switch_to.frame(frame)
+        self.browser.find_element(*MainPage.EMAIL_FIELD).send_keys("costartrial")
 
     def click_enter_password_button(self):
-        self.browser.find_element(*locators.ENTER_PASSWORD_BUTTON).click()
+        self.browser.find_element(*MainPage.ENTER_PASSWORD_BUTTON).click()
 
     def enter_password_button(self):
-        self.browser.find_element(*locators.PASSWORD_FIELD).send_keys("Costar12345!")
+        self.browser.find_element(*MainPage.PASSWORD_FIELD).send_keys("Costar12345!")
 
     def click_login_button_password(self):
-        self.browser.find_element(*locators.LOGIN_BUTTON_PASSWORD).click()
+        self.browser.find_element(*MainPage.LOGIN_BUTTON_PASSWORD).click()
 
 
 
