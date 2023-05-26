398d083f20b38318b322f868d2b15d2c2a265605
katyasamuseva <katsiaryna.samuseva@cloudcommercegroup.com>
2023-05-26 11:48:26 +0200

    no message

--- conftest.py
+++ conftest.py
@@ -8,4 +8,4 @@ def browser():
     driver = webdriver.Chrome(ChromeDriverManager().install()) # explain this line for me
     driver.implicitly_wait(10)
     yield driver
-    time.sleep(40) # should avoid hard waits, especially sleep(). this line should be deleted. you're basically wait for 40 second after each test
+    driver.close()
