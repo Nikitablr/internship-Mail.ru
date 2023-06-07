d5e199d56532fa0f4ae9a9278c01f3a2ab2ce758
nikrud <89258843+nikrud@users.noreply.github.com>
2023-06-07 12:11:38 +0200

    api tests

---
+++ tests_api/test_api.py
@@ -0,0 +1,25 @@
+import requests
+import random
+
+
+def test_get_request():
+    response = requests.get("https://api.chucknorris.io/jokes/random")
+    assert response.status_code == 200
+    print(response.text)
+
+
+def test_get_random_categories():
+    categories = ["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music",
+                  "political","religion","science","sport","travel"]
+    random_categories = random.choice(categories)
+    response = requests.get(f"https://api.chucknorris.io/jokes/random?category={random_categories}")
+    print(response.json())
+    assert response.status_code == 200
+
+
+def test_get_categories():
+    response = requests.get("https://api.chucknorris.io/jokes/categories")
+    print(response.json())
+    assert response.status_code == 200
+
+
