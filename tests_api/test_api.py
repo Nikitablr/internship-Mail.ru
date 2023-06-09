b05d17184e45ab7467aa69f2756ff56296701c6b
nikrud <89258843+nikrud@users.noreply.github.com>
2023-06-09 14:38:17 +0200

    fix tests

--- tests_api/test_api.py
+++ tests_api/test_api.py
@@ -1,25 +1,41 @@
 import requests
-import random
+from tests_api import random_select_categorie
 
 
 def test_get_request():
     response = requests.get("https://api.chucknorris.io/jokes/random")
     assert response.status_code == 200, 'Wrong status code'
-    print(response.text)
+
+
+def test_get_request_check_value():
+    response = requests.get("https://api.chucknorris.io/jokes/random")
+    j = response.json()
+    assert j["value"] is not None
 
 
 def test_get_random_categories():
-    categories = ["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music",
-                  "political","religion","science","sport","travel"]
-    random_categories = random.choice(categories)
-    response = requests.get(f"https://api.chucknorris.io/jokes/random?category={random_categories}")
-    print(response.json())
-    assert response.status_code == 200, 'Wrong status code'
+    r = requests.get(f'https://api.chucknorris.io/jokes/random?category={random_select_categorie.random_select_categories()}')
+    assert r.status_code == 200, 'Wrong status code'
+
+
+
+def test_check_categorie_in_response():
+    r = random_select_categorie.random_select_categories()
+    response = requests.get(f'https://api.chucknorris.io/jokes/random?category'
+                      f'={r}')
+    j = response.json()
+    assert r in j['categories']
 
 
 def test_get_categories():
     response = requests.get("https://api.chucknorris.io/jokes/categories")
-    print(response.json())
     assert response.status_code == 200, 'Wrong status code'
+
+
+def test_number_of_elements_in_categories():
+    response = requests.get("https://api.chucknorris.io/jokes/categories")
+    res = response.json()
+    assert len(res) == 16
+
 
 
