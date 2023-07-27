354ff3ec0462762fa3cd378ce02533de705f9614
nikrud <89258843+nikrud@users.noreply.github.com>
2023-07-27 15:53:02 +0200

    added selenoid caps

--- conftest.py
+++ conftest.py
@@ -1,11 +1,30 @@
 import pytest
 from selenium import webdriver
-import time
-from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "106.0",
+    "selenoid:options": {
+        "enableVNC": True,
+        "enableVideo": True
+    }
+}
+
 
 @pytest.fixture(scope="function")
 def browser():
-    driver = webdriver.Chrome(ChromeDriverManager().install()) # explain this line for me
-    driver.implicitly_wait(10)
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub",
+        desired_capabilities=capabilities)
     yield driver
     driver.close()
+
+# @pytest.fixture(scope="function")
+# def browser():
+#     driver = webdriver.Chrome()
+#     driver.implicitly_wait(10)
+#     yield driver
+#     driver.close()
+
